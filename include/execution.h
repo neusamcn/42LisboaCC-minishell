d63fac579ca6a0a4ced2e950eb8c90f41c6a759d
megiazavr <milena.egiazaryan77@mail.ru>
2026-04-01 20:27:05 +0100

    struct upd

--- include/execution.h
+++ include/execution.h
@@ -6,7 +6,7 @@
 /*   By: megi <megi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 15:46:35 by megi              #+#    #+#             */
-/*   Updated: 2026/03/30 21:15:59 by megi             ###   ########.fr       */
+/*   Updated: 2026/04/01 20:19:13 by megi             ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -33,35 +33,76 @@
 # define EXPORT "export"
 # define UNSET "unset"
 
+int g_signal = 0;
+
+typedef enum e_fds
+{
+	STDIN,
+	STDOUT,
+}   t_fds;
+
 typedef enum e_builts
 {
 	BUILTINS,
 	NON_BUILTINS
-}	builtins_check;
+}	t_builtins_check;
 
-// [ls] → [grep src] → [wc -l] → NULL 
+typedef enum e_redir_type
+{
+	IN,
+	OUT,
+	APPEND,
+	HEREDOC,
+	FD,
+}	t_redir_type;
+
+typedef struct s_redirections
+{
+	t_redir_type	type;
+	char	*filename;
+    char    *delimiter;
+	struct s_redirections	*next;
+}   t_redirects;
 
 typedef struct s_pipe
 {
     char            **cmds;
-    char            *infile;
-    char            *outfile;
-    int             append;
+    t_redirects     redir;
     struct s_pipe   *next;
 }   t_pipe;
 
+/* typedef struct s_execution
+{
+	t_builtin_cmd		builtins[12];
+	char				*cmd_name;
+	int					*pid;
+}	t_execution; */
+
+// [ls] → [grep src] → [wc -l] → NULL 
+
 // PATH //
-char *path(t_pipe *cmd_line, char **envp);
-char *paths_helper(t_pipe *cmd_line, char **envp, int i);
-char *absolute_path(t_pipe *cmd_line);
-int free_path(char **paths);
+char 	*path(t_pipe *cmd_line, char **envp);
+char 	*paths_helper(t_pipe *cmd_line, char **envp, int i);
+char 	*absolute_path(t_pipe *cmd_line);
+int 	free_path(char **paths);
 
 // FORKs and SIGNALs
-int exec(t_pipe *cmd_line, char **envp, int status);
-void signal_handler(pid_t pid);
-int status_check(int status);
-int are_you_builtin(t_pipe *cmd_line);
+int		exec(t_pipe *cmd_line, char **envp, int status);
+void	child_exec(char *cmd, t_pipe *cmd_line, char **envp);
+void	signal_handler(pid_t pid);
+int		status_check(int status);
+int		are_you_builtin(t_pipe *cmd_line);
 
 // PIPEs //
+/* void 	pipe_handler(t_pipe *cmd_line);
+void 	heredoc(t_pipe *cmd_line);
+void append(t_pipe *cmd_line); */
+
+void heredoc(t_redirects *heredoc);
+void signal_heredoc(t_redirects *heredoc_signals);
+void append(t_redirects *append);
+void pipe_handler(t_redirects *in_or_out);
+
 
 # endif
+
