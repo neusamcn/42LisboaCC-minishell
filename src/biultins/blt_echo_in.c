adf081b2ba77086236395d659df72a0f631c00a8
Milena Egiazarian <megiazar@c1r10s2.42lisboa.com>
2026-04-22 19:20:36 +0100

    got git pulling

---
+++ src/biultins/blt_echo_in.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   blt_echo_ins.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: megi <megi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 16:24:22 by megi              #+#    #+#             */
+/*   Updated: 2026/04/22 16:53:22 by megi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "execution.h"
+
+//printing msges to stdout 
+// wildcart character * to output multiple files (ike cat hello*)
+
+// print everything that i pass as a third args untill any redirections
+// untill a newline
+
+// getchar and putchar till receive a newline
+int myecho(t_bltn *echo, char **envp)
+{
+    char whoareyou;
+
+    whoareyou = echo;
+    while (whoareyou != '\n')
+    {
+        ft_putchar_fd(whoareyou);
+    }
+    return (0);
+}
