fd8b051a4839814fbfef59a183f2507ae691b7a2
megiazavr <milena.egiazaryan77@mail.ru>
2026-04-22 17:28:47 +0100

    bltn start

---
+++ src/biultins.c/blt_run_ins.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex_bltn_ecution.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: megi <megi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 21:00:17 by megi              #+#    #+#             */
+/*   Updated: 2026/04/18 23:37:11 by megi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+
+int are_you_builtin(t_cmd_line *cmd_line)
+{
+	if (!cmd_line || !cmd_line->cmds || !cmd_line->cmds[0])
+        return (EXTRENAL);
+	if ((!ft_strcmp(cmd_line->cmds[0], CD)) || (!ft_strcmp(cmd_line->cmds[0], ECHO)) || (!ft_strcmp(cmd_line->cmds[0], EXIT)) ||
+		(!ft_strcmp(cmd_line->cmds[0], PWD)) || (!ft_strcmp(cmd_line->cmds[0], ENV)) || (!ft_strcmp(cmd_line->cmds[0], EXPORT)) ||
+		(!ft_strcmp(cmd_line->cmds[0], UNSET)))
+		return (BUILTINS);
+	return (EXTRENAL);
+}
+
