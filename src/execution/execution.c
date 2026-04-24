13821a44efc7e1df35d8db44c41dd885ef316a3d
Milena Egiazarian <megiazar@c2r1s2.42lisboa.com>
2026-04-24 16:07:49 +0100

    bltn change

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -12,84 +12,85 @@
 
 #include "execution.h"
 
-static void	no_cmds_execution(t_cmd_line *cmds, char **envp)
+static void no_cmds_execution(t_cmd_line *cmds, char **envp)
 {
-    t_redirects *redir;
+	t_redirects *redir;
 	int save_out;
 
 	(void)envp;
 	if (!cmds->cmds || !cmds->cmds[0])
 	{
-    	save_out = dup(WRITE);
-   		redir = &cmds->redir;
-    	while (redir && redir->type != NONE)
-    	{
-        	if (redir->type == OUT || redir->type == APPEND)
-            	append(redir);
+		save_out = dup(1);
+		if (save_out == -1)
+			return;
+		redir = &cmds->redir;
+		while (redir && redir->type != NONE)
+		{
+			if (redir->type == OUT || redir->type == APPEND)
+				append(redir);
 			else if (redir->type == IN)
 				in_redir(redir);
-        	redir = redir->next;
-    	}
-    	dup2(save_out, WRITE);
-    	close(save_out);
-    	return ;
+			redir = redir->next;
+		}
+		dup2(save_out, 1);
+		close(save_out);
 	}
 }
 
 void exec_loop(t_cmd_line *cmds, char **envp)
 {
-    t_cmd_line  *tmp;
-    t_redirects *redir;
+	t_cmd_line  *tmp;
+	t_redirects *redir;
 
-    tmp = cmds;
-    while (tmp)
-    {
-        redir = &tmp->redir;
-        while (redir && redir->type != NONE)
-        {
-            if (redir->type == HEREDOC)
-                heredoc(redir);
-            redir = redir->next;
-        }
-        tmp = tmp->next;
-    }
+	tmp = cmds;
+	while (tmp)
+	{
+		redir = &tmp->redir;
+		while (redir && redir->type != NONE)
+		{
+			if (redir->type == HEREDOC)
+				heredoc(redir);
+			redir = redir->next;
+		}
+		tmp = tmp->next;
+	}
 	if (!cmds->cmds || !cmds->cmds[0])
 		no_cmds_execution(cmds, envp);
-    if (cmds->next == NULL && are_you_builtin(cmds) == BUILTINS)
-        lonely_blt(cmds, envp);
-    else if (cmds->next == NULL)
-        mommy_n_father(cmds, envp);
-    else
-        ex_pipeline_ec(cmds, envp);
+	if (cmds->next == NULL && are_you_builtin(cmds) == BUILTINS)
+		lonely_blt(cmds, envp);
+	else if (cmds->next == NULL)
+		mommy_n_father(cmds, envp);
+	else
+		ex_pipeline_ec(cmds, envp);
 }
 
 int lonely_blt(t_cmd_line *s, char **envp)
 {
+    int read_save;
+    int write_save;
+
     (void)envp;
+    read_save = dup(0);
+    write_save = dup(1);
+    if (read_save == -1 || write_save == -1)
+        return (perror("dup"), 1);
     if (if_redir(s))
     {
-        s->redir.fd[0] = dup(READ);
-        s->redir.fd[1] = dup(WRITE);
-        if (s->redir.fd[0] == -1 || s->redir.fd[1] == -1)
-            return (perror("dup"), 1);
         if (do_redri(&s->redir) != 0)
         {
-            dup2(s->redir.fd[0], READ);
-            dup2(s->redir.fd[1], WRITE);
-            close(s->redir.fd[0]);
-            close(s->redir.fd[1]);
+            dup2(read_save, 0);
+            dup2(write_save, 1);
+            close(read_save);
+            close(write_save);
             return (1);
         }
     }
     sig_mode(BLT_EXECUTING);
-    // r_bltn(s);
-    if (if_redir(s))
-    {
-        dup2(s->redir.fd[0], READ);
-        dup2(s->redir.fd[1], WRITE);
-        close(s->redir.fd[0]);
-        close(s->redir.fd[1]);
-    }
+    r_bltn(s, envp);
+    dup2(read_save, 0);
+    dup2(write_save, 1);
+    close(read_save);
+    close(write_save);
     set_signals_interactive_parent();
     return (get_signal_stat());
 }
@@ -115,17 +116,17 @@ int single_child_ex(t_cmd_line *kid, char **envp)
 {
 	char *path;
 
-    sig_mode(CHILD);
-    if (if_redir(kid) && do_redri(&kid->redir) != 0)
-        exit(1);
+	sig_mode(CHILD);
+	if (if_redir(kid) && do_redri(&kid->redir) != 0)
+		exit(1);
 	path = abs_or_rel_p(kid, envp);
 	if (!path)
 	{
 		mndp_log_err("commad not found\n", kid->cmds[0]);
 		exit (127);
 	}
-    execve(path, kid->cmds, envp);
+	execve(path, kid->cmds, envp);
 	free (path);
-    mndp_log_err("Execution failed!\n", kid->cmds[0]);
-    exit(127);
+	mndp_log_err("Execution failed!\n", kid->cmds[0]);
+	exit(127);
 }
